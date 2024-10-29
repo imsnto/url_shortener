ef3cfef424ae4783d21848814b79de9ab8ca59ba
Solaiman Hossain <slmnsnto@gmail.com>
2024-10-29 16:37:59 +0600

    update URL model unless raise error if same url generates

--- shortener/models.py
+++ shortener/models.py
@@ -8,7 +8,7 @@ class URL(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.short_url:
-            self.short_url = self.generate_short_url()
+            self.short_url = self.generate_unique_short_url()
 
         super().save(*args, **kwargs)
     
@@ -16,3 +16,9 @@ class URL(models.Model):
         characters = string.ascii_letters + string.digits
         short_url = ''.join(random.choice(characters) for i in range(10))
         return short_url
+    
+    def generate_unique_short_url(self):
+        while True:
+            short_url = self.generate_short_url()
+            if not URL.objects.filter(short_url=short_url).exists():
+                return short_url
